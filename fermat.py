2fda244c6b59163f9fd1b6dd69b722e5ab98a702
Vaibhav Jayant <43247883+d4rkvaibhav@users.noreply.github.com>
2019-03-20 15:51:19 +0530

    Update fermat.py

--- fermat.py
+++ fermat.py
@@ -1,15 +1,28 @@
-import math
+def isqrt(n):
+  x = n
+  y = (x + n // x) // 2
+  while y < x:
+    x = y
+    y = (x + n // x) // 2
+  return x
+
+
+def fermat(n):
+	t0=isqrt(n)+1
+	counter=0
+	t=t0+counter
+	temp=isqrt((t*t)-n)
+	while((temp*temp)!=((t*t)-n)):
+		counter+=1
+		t=t0+counter
+		temp=isqrt((t*t)-n)
+	s=temp
+	p=t+s
+	q=t-s
+	return p,q
+
 print("Enter the number to factor of form (p*q):	")
 n=int(input())
-t0=math.ceil((math.sqrt(n)))
-counter=0
-ans=math.sqrt(pow((t0+counter),2)-n)
-while(not float(ans).is_integer()):
-	counter+=1
-	ans=math.sqrt(pow((t0+counter),2)-n)
-t=t0+counter
-s=ans
-p=t-s
-q=t+s
+p,q=fermat(n)
 print("Your first number   : ",int(p))
 print("Your Second number  : ",int(q))
